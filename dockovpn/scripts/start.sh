d8de02ce071bcf4ca78735c444530c9b189423fe
glowfisch <glowfisch8lan@gmail.com>
2023-07-07 19:41:38 +1000

    fix docker version

--- dockovpn/scripts/start.sh
+++ dockovpn/scripts/start.sh
@@ -52,7 +52,7 @@ yes
 EOF3
     # Certificate created at: /opt/Dockovpn_data/pki/issued/pvpn.crt
 
-    openvpn --genkey --secret ta.key << EOF4
+    openvpn --genkey secret ta.key << EOF4
 yes
 EOF4
 
